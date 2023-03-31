460603e16b2eff2a8e68b1e07286775530ffaa06
Mikka Kantinti <mikka.kantinti@epitech.digital>
2023-03-31 13:50:00 +0200

    [updated header and body with menu Items]

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "./Header.css"
+import Logo from "./logo.svg"
 
 function Header() {
   return (
-    <div className='Header__Wrapper'>
-        
+    <div className='header__Wrapper'>
+        <div className='header__logo'>
+            <img src={Logo} alt="Logo" width={25}/>
+        </div>
+        <div className='header__search'>
+            <div className="header_searchContainer">
+                <input placeholder='Recherche' type="text" />
+            </div>
+        </div>
+        <div className='header_menuItems'>
+            <a href='#'>Stocks Gratuit</a>
+            <a href='#'>Portfolio</a>
+            <a href='#'>Cash</a>
+            <a href='#'>Messages</a>
+            <a href='#'>Compte</a>
+        </div>
     </div>
   )
 }
