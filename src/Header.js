f59882c9786d09f2f4aa846d3dfd1c8cbc36883a
Mikka Kantinti <mikka.kantinti@epitech.digital>
2023-03-31 14:28:04 +0200

    [updated css]

--- src/Header.js
+++ src/Header.js
@@ -4,13 +4,13 @@ import Logo from "./logo.svg"
 
 function Header() {
   return (
-    <div className='header__Wrapper'>
+    <div className='header__wrapper'>
         <div className='header__logo'>
             <img src={Logo} alt="Logo" width={25}/>
         </div>
         <div className='header__search'>
-            <div className="header_searchContainer">
-                <input placeholder='Recherche' type="text" />
+            <div className="header__searchContainer">
+                <input placeholder='Recherche' type="text"/>
             </div>
         </div>
         <div className='header_menuItems'>
