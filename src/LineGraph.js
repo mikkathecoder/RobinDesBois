04ae2fb90b33f97e7d66a4ac5cd61dfd69c3c5de
Mikka Kantinti <mikka.kantinti@epitech.digital>
2023-04-01 10:52:16 +0200

    [created LineGraph]

---
+++ src/LineGraph.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function LineGraph() {
+  return (
+    <div>LineGraph</div>
+  )
+}
+
+export default LineGraph
--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./Newsfeed.css"
+import LineGraph from './LineGraph'
 
 function Newsfeed() {
   return (
     <div className='newsfeed'>
         <div className="newsfeed__container">
-            
+            <div className='newsfeed__chartSection'>
+                <div className='newsfeed__portfolio'>
+                    <h1>€3,584,102.255</h1>
+                    <p>+€2,021.621 (+0.01%) AJD</p>
+                </div>
+                <div className='newsfeed__chart'>
+                    <LineGraph />
+                </div>
+            </div>
         </div>
     </div>
   )
