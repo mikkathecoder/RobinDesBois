0e0779119de7433b53bf43f07c56d4bf5dd5769a
Mikka Kantinti <mikka.kantinti@epitech.digital>
2023-04-04 04:45:54 +0200

    [fixing chip issue]

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState} from 'react'
 import "./Newsfeed.css"
 import LineGraph from './LineGraph'
 import TimeLine from "./TimeLine"
+import Chip from '@material-ui/core/Chip';
+import { Avatar } from '@material-ui/core';
 
 function Newsfeed() {
+  const [popularTopics, setTopics] = useState([
+    "Technology",
+    "Top Movies",
+    "Upcoming Earnings",
+    "Crypto",
+    "Cannabis",
+    "Healthcare Supplies",
+    "Index ETFs",
+    "Technology",
+    "China",
+    "Pharma",
+  ]);
+
   return (
     <div className='newsfeed'>
         <div className="newsfeed__container">
@@ -27,6 +42,24 @@ function Newsfeed() {
                 <h1>Bonjour chers investisseur :D</h1>
               </div>
             </div>
+            <div className="newsfeed__popularlists__section">
+              <div className="newsfeed__popularlists__intro">
+                <h1> En Tendance</h1>
+                <p>Voir plus</p>
+              </div>
+              <div className="newsfeed_popularlists_badges">
+                {popularTopics.map((topic) => (
+                  <Chip 
+                    className="topic__badge"
+                    variant="outlined"
+                    label={topic}
+                    avatar={<Avatar
+                    src={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                  />} 
+               />
+              ))}
+           </div>
+          </div>
         </div>
     </div>
   )
